54275bd1fbbd0b31d03162861b0dc61481220376
Calvin Chu <calvinhoyinchu@gmail.com>
2020-07-01 22:08:34 +0800

    updated home page, added login page

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 # library.py
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import sql
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home</h1>"
+    return render_template('home.html')
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+
+
+
+'''
+app.route('/medications/<med>')
+def show_med_information(med):
+    # show the user profile for that user
+    return 'User %s' % escape(med)
+'''
 
 
 @app.route("/about")
 def about():
-    return "About!"
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
