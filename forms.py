b45019d0e22b8d050fa63ea561df467550126843
Calvin Chu <calvinhoyinchu@gmail.com>
2020-09-01 23:34:17 +0800

    added LoginForm, Register From

--- forms.py
+++ forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, PasswordField, SelectField
+from wtforms.validators import DataRequired, InputRequired, Length, Email
 
 
-class register(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    profession = SelectField('Profession', choices=[('1', 'Physician'), ('2', 'Pharmacist'), ('3', 'Nurse Practioner')])
+    department = StringField('Department', validators=[DataRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
