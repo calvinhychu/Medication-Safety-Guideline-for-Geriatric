6196869ff7bfaf13840d52ec3e145b4fdd351f11
Calvin Chu <calvinhoyinchu@gmail.com>
2020-09-20 04:53:59 +0800

    .

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template, request, flash, session, redirect, \
 from flask_bootstrap import Bootstrap
 from forms import *
 from medication import *
-from models import *
+# from models import *
 from flask_sqlalchemy  import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_mail import Mail, Message
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired
@@ -27,6 +28,38 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    profession = db.Column(db.String(), nullable=False)
+    department = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(80))
+    notes = db.relationship('Note', backref='user')
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    drug_id = db.Column(db.Integer, db.ForeignKey('drug.id'))
+    drugclass_id = db.Column(db.Integer, db.ForeignKey('drugclass.id'))
+    
+class Drugclass(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True, nullable=False)
+    beers_criteria = db.Column(db.Text, nullable=True)
+    stopp_start_criteria = db.Column(db.Text, nullable=True)
+    drugs = db.relationship('Drug', backref='drugclass')
+    notes = db.relationship('Note', backref='drugclass')
+
+class Drug(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True, nullable=False)
+    beers_criteria = db.Column(db.Text, nullable=True)
+    stopp_start_criteria = db.Column(db.Text, nullable=True)
+    drug_class_id = db.Column(db.Integer, db.ForeignKey('drugclass.id')) 
+    notes = db.relationship('Note', backref='drug')
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -197,22 +230,22 @@ def submit_notes(category, drug):
 def about():
     return render_template('about.html', load_user=load_user)
 
-# if __name__ == "__main__":
-#     # db.create_all()
-#     # for a in drug_classes.keys():
-#     #     new_drug_class = Drugclass(name=a, beers_criteria=drug_classes[a][0],
-#     #     stopp_start_criteria=drug_classes[a][1])
-#     #     try:
-#     #         db.session.add(new_drug_class)
-#     #         db.session.commit()
-#     #     except:
-#     #         pass
-#     # for a in medications.keys():
-#     #     new_med = Drug(name=a, beers_criteria=medications[a][0],
-#     #     stopp_start_criteria=medications[a][1], drug_class_id=medications[a][2])
-#     #     try:
-#     #         db.session.add(new_med)
-#     #         db.session.commit()
-#     #     except:
-#     #         pass
-#     app.run()
+if __name__ == "__main__":
+    # db.create_all()
+    # for a in drug_classes.keys():
+    #     new_drug_class = Drugclass(name=a, beers_criteria=drug_classes[a][0],
+    #     stopp_start_criteria=drug_classes[a][1])
+    #     try:
+    #         db.session.add(new_drug_class)
+    #         db.session.commit()
+    #     except:
+    #         pass
+    # for a in medications.keys():
+    #     new_med = Drug(name=a, beers_criteria=medications[a][0],
+    #     stopp_start_criteria=medications[a][1], drug_class_id=medications[a][2])
+    #     try:
+    #         db.session.add(new_med)
+    #         db.session.commit()
+    #     except:
+    #         pass
+    app.run()
