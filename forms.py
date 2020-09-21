5a292c9c59d09a33ff27a39ce7041e053c84c20e
Calvin Chu <calvinhoyinchu@gmail.com>
2020-09-21 20:58:07 +0800

    added note forms

--- forms.py
+++ forms.py
@@ -2,10 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, InputRequired, Length, Email, ValidationError
 
+#Change this field if you would like to restrain only a certain email domain will be able to register for an account
 def check_email_domain(form, field):
     if field.data[-17:] != '@stjoestoronto.ca':
         raise ValidationError('Email must be under domain of @stjoestoronto.ca')
 
+#Add it under check_email_domain() inside the validators list of email 
 class RegisterForm(FlaskForm):
     name = StringField('Name', validators=[InputRequired()])
     profession = SelectField('Profession', choices=[('Physician', 'Physician'), ('Pharmacist', 'Pharmacist'), ('Nurse Practioner', 'Nurse Practioner')])
